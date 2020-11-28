b147d5e44a659221b45f0be9884891209be19ff8
dgngulcan <dgngulcan@gmail.com>
2020-11-27 21:28:35 -0500

    initial commit

---
+++ code.ts
@@ -0,0 +1,31 @@
+figma.showUI(__html__, { visible: true, width: 400, height: 450 });
+
+
+// limitations
+// single font
+figma.ui.onmessage = msg => {
+  if (msg.type === 'add-pages') {
+    var currentIndex = msg.startIndex
+
+
+    figma
+      .currentPage
+      .findAll(node => node.type === "TEXT" && node.characters === "{pageLabel}")
+      .forEach(function (node) {
+        if (node.type === "TEXT") {
+          var newText = msg.suffix + currentIndex + msg.postfix
+
+          figma.loadFontAsync(node.fontName)
+            .then(function () {
+              node.characters = newText
+            }, function () {
+
+            })
+
+          currentIndex = currentIndex + 1
+        }
+      })
+  }
+
+  figma.closePlugin();
+};
