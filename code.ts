ceaad3324ca599361f7e8e86aa49e076a67e4a60
dgngulcan <dgngulcan@gmail.com>
2021-01-18 17:05:38 -0500

    mvp

--- code.ts
+++ code.ts
@@ -2,26 +2,29 @@ figma.showUI(__html__, { visible: true, width: 400, height: 450 });
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'add-pages') {
-    var currentIndex = msg.startIndex
-
-    figma
-      .currentPage
-      .findAll(node => node.type === "TEXT" && node.characters === "{p#}")
-      .forEach(function (node) {
-        if (node.type === "TEXT") {
-          var newText = msg.prefix + currentIndex + msg.suffix
-
-          figma.loadFontAsync(node.fontName)
-            .then(function () {
-              node.characters = newText
-            }, function () {
-
-            })
-
-          currentIndex = currentIndex + 1
-        }
-      })
+    addPageNumbers(msg)
   }
 
   figma.closePlugin();
+};
+
+function addPageNumbers(message) {
+  var currentIndex = message.startIndex
+
+  figma
+    .currentPage
+    .findAll(node => node.type === "TEXT")
+    .filter(node => node.characters === "{p#}")
+    .forEach(function (node) {
+      var newText = message.prefix + currentIndex + message.suffix
+
+      figma.loadFontAsync(node.fontName)
+        .then(function () {
+          node.characters = newText
+        }, function () {
+
+        })
+
+      currentIndex++
+    })
 };
